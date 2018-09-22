4494c5f18977229cc98ae1d1798b1882722fc8b2
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-22 04:38:07 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,12 +5,12 @@ describe 'new post' do
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
-  
+
   it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
   end
-  
+
   it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
     visit new_post_path
     fill_in 'post_title', with: 'My post title'
