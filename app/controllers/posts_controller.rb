8abd0ca5d21e249d902f72e5df00b050b0deda45
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-22 04:39:32 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def new
   end
-  
+
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
